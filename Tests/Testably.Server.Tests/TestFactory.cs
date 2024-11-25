f87398f7fa1cd232e4dab7ca890391dce2f7accc
Valentin <valentin.breuss@baur.eu>
2024-11-25 09:38:25 +0100

    Fix tests

--- Tests/Testably.Server.Tests/TestFactory.cs
+++ Tests/Testably.Server.Tests/TestFactory.cs
@@ -1,17 +1,25 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.AspNetCore.TestHost;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
 namespace Testably.Server.Tests;
 
 public class TestFactory : WebApplicationFactory<Program>
 {
+    private readonly IConfiguration _configuration;
     private readonly IHttpClientFactory? _httpClientFactory;
 
-    public TestFactory(IHttpClientFactory? httpClientFactory = null)
+    public TestFactory(IHttpClientFactory? httpClientFactory = null,
+        Action<Dictionary<string, string>>? configuration = null)
     {
         _httpClientFactory = httpClientFactory;
+        var builder = new ConfigurationBuilder();
+        var inMemoryConfiguration = new Dictionary<string, string>();
+        configuration?.Invoke(inMemoryConfiguration);
+        _configuration = builder.AddInMemoryCollection(inMemoryConfiguration!).Build();
     }
 
     /// <inheritdoc />
@@ -22,8 +30,8 @@ public class TestFactory : WebApplicationFactory<Program>
         {
             if (_httpClientFactory != null)
             {
-                var descriptor =
-                    services.SingleOrDefault(d => d.ServiceType == _httpClientFactory.GetType());
+                var descriptor = services
+                    .SingleOrDefault(d => d.ServiceType == _httpClientFactory.GetType());
                 if (descriptor != null)
                 {
                     services.Remove(descriptor);
@@ -31,6 +39,17 @@ public class TestFactory : WebApplicationFactory<Program>
 
                 services.AddSingleton(_httpClientFactory);
             }
+
+            services.AddSingleton(_configuration);
         });
+    }
+
+    /// <summary>
+    ///     https://stackoverflow.com/a/69825605
+    /// </summary>
+    protected override IHost CreateHost(IHostBuilder builder)
+    {
+        builder.UseContentRoot(Directory.GetCurrentDirectory());
+        return base.CreateHost(builder);
     }
 }
